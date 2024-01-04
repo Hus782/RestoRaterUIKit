c294d539c068dd5511dbf7ff1d816764a70e565f
Hyusein Hyusein <hyusein.ali.hyusein98@gmail.com>
2024-01-04 12:24:47 +0100

    Added documentation for UserManager

--- RestoRaterUIKit/Persistance/UserManager.swift
+++ RestoRaterUIKit/Persistance/UserManager.swift
@@ -7,13 +7,14 @@
 
 import Foundation
 
+/// Struct representing the user's data.
 struct UserData {
     let name: String
     let email: String
     let isAdmin: Bool
 }
 
-// Expose the important methods for testing and mocking
+/// Protocol to define the key functionalities of the UserManager, useful for testing and mocking.
 protocol UserManagerProtocol {
     func loginUser(user: User)
     func isCurrentUser(user: User) -> Bool
@@ -21,12 +22,13 @@ protocol UserManagerProtocol {
     func setIsRegistering(_ value: Bool)
 }
 
+/// Singleton class responsible for managing user authentication, session state, and user data.
 final class UserManager: ObservableObject, UserManagerProtocol {
     @Published var currentUser: UserData?
     @Published var isLoggedIn: Bool = false
     @Published private(set) var isRegistering: Bool = true
     
-    // Keys for UserDefaults
+    // Keys used for storing data in UserDefaults
     private let isLoggedInKey = "isLoggedIn"
     private let isAdminKey = "isAdminKey"
     private let userEmailKey = "userEmail"
@@ -35,35 +37,36 @@ final class UserManager: ObservableObject, UserManagerProtocol {
     // Singleton instance for global access
     static let shared = UserManager()
     
-    // Private constructor for singleton
+    // Private constructor to enforce singleton usage
     private init() {
         loadUserFromDefaults()
     }
     
+    /// Checks if the specified user is the current user.
     func isCurrentUser(user: User) -> Bool {
-        if let currentName = currentUser?.name, let currentEmail = currentUser?.email, currentName == user.name, currentEmail == user.email {
-            return true
-        } else {
-            return false
-        }
+        return currentUser?.name == user.name && currentUser?.email == user.email
     }
-
+    
+    /// Logs in the user and persists the user data.
     func loginUser(user: User) {
         currentUser = UserData(name: user.name, email: user.email, isAdmin: user.isAdmin)
         isLoggedIn = true
         saveUserToDefaults()
     }
     
+    /// Logs out the current user and clears persisted data.
     func logoutUser() {
-        self.currentUser = nil
-        self.isLoggedIn = false
+        currentUser = nil
+        isLoggedIn = false
         clearUserDefaults()
     }
     
+    /// Sets the registration status for the current session.
     func setIsRegistering(_ value: Bool) {
         isRegistering = value
     }
     
+    // Persists user data to UserDefaults
     private func saveUserToDefaults() {
         UserDefaults.standard.set(isLoggedIn, forKey: isLoggedInKey)
         UserDefaults.standard.set(currentUser?.email, forKey: userEmailKey)
@@ -71,25 +74,22 @@ final class UserManager: ObservableObject, UserManagerProtocol {
         UserDefaults.standard.set(currentUser?.isAdmin, forKey: isAdminKey)
     }
     
+    // Loads user data from UserDefaults
     private func loadUserFromDefaults() {
-        let isLoggedIn = UserDefaults.standard.bool(forKey: isLoggedInKey)
+        isLoggedIn = UserDefaults.standard.bool(forKey: isLoggedInKey)
         if isLoggedIn {
             let email = UserDefaults.standard.string(forKey: userEmailKey) ?? ""
             let name = UserDefaults.standard.string(forKey: userNameKey) ?? ""
             let isAdmin = UserDefaults.standard.bool(forKey: isAdminKey)
-
-            // Create a user object with the stored values
-            let user = UserData(name: name, email: email, isAdmin: isAdmin)
-            self.currentUser = user
-            self.isLoggedIn = true
+            currentUser = UserData(name: name, email: email, isAdmin: isAdmin)
         }
     }
     
+    // Clears user data from UserDefaults
     private func clearUserDefaults() {
         UserDefaults.standard.removeObject(forKey: isLoggedInKey)
         UserDefaults.standard.removeObject(forKey: userEmailKey)
         UserDefaults.standard.removeObject(forKey: userNameKey)
         UserDefaults.standard.removeObject(forKey: isAdminKey)
     }
-    
 }
